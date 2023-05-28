685ce6d467b0f7df4ed0af3210f76c8f5aa1efa6
Bryan Neveu <b.neveu09@gmail.com>
2023-05-28 18:51:32 +0200

    add : last step new compositions

--- src/components/compositions/validation/CompositionValidation.jsx
+++ src/components/compositions/validation/CompositionValidation.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import useLocalStorage from '../../../hooks/useLocalStorage'
-import { redirect } from "react-router-dom";
+import { redirect } from 'react-router-dom'
 
 function CompositionValidation ({ onPrevious, picks, bans }) {
   const [compositionName, setCompositionName] = useState('')
@@ -34,7 +34,7 @@ function CompositionValidation ({ onPrevious, picks, bans }) {
     })
 
     // Redirection vers la page des compositions
-    return redirect("/compositions");
+    return redirect('/compositions')
   }
 
   // Génère un ID unique pour la composition (à titre d'exemple)
@@ -43,25 +43,39 @@ function CompositionValidation ({ onPrevious, picks, bans }) {
   }
 
   return (
-    <div>
-      {/* Formulaire pour saisir le nom de la composition */}
-      <label>Choisir un nom pour votre composition*</label>
-      <input
-        type='text'
-        value={compositionName}
-        onChange={e => setCompositionName(e.target.value)}
-      />
-
-      {/* Affichage des erreurs */}
-      {compositionName === '' && (
-        <p>Veuillez saisir un nom pour la composition.</p>
-      )}
+    <div className='new-composition__inner'>
+      <div
+        className='input__container'
+        style={{
+          justifyContent: 'center'
+        }}
+      >
+        <label className='input__label h2'>
+          Choisir un nom pour votre composition<sup>*</sup>
+        </label>
+        <input
+          type='text'
+          value={compositionName}
+          onChange={e => setCompositionName(e.target.value)}
+          className='input__input'
+        />
+        {/* Affichage helper */}
+        {compositionName === '' && (
+          <p className='input__helper small'>
+            Veuillez saisir un nom pour la composition.
+          </p>
+        )}
+      </div>
 
       {/* Boutons pour passer à l'étape précédente ou compléter la création */}
-      <button onClick={onPrevious}>Previous</button>
-      <button onClick={handleComplete} disabled={compositionName === ''}>
-        Complete
-      </button>
+
+      <div className='new-composition__buttons'>
+        <button onClick={onPrevious}>Previous</button>
+        <span className='new-composition__buttons-separator'></span>
+        <button onClick={handleComplete} disabled={compositionName === ''}>
+          Sauvegarder
+        </button>
+      </div>
     </div>
   )
 }
